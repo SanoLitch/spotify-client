6190f7aeb2448de40de0d5b0552d25408b75653c
SanoLitch <sanolitch@gmail.com>
2026-02-19 19:20:05 +0300

    feat(backend): Add CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import {
   DocumentBuilder, SwaggerModule,
 } from '@nestjs/swagger';
@@ -7,24 +8,40 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
+const API_PREFIX = 'api';
+const DOC_PREFIX = 'docs';
+
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.use(cookieParser());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(API_PREFIX);
+
+  // Enable CORS
+  app.enableCors({
+    origin: configService.get<string>('FE_URI') || 'http://localhost:3000',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Spotify Client API')
-    .setDescription('The Spotify Client application API description')
+    .setDescription(
+      'The Spotify Client application API description. ' +
+      '<br/><br/>' +
+      '<b>Note:</b> To test protected endpoints, you must first log in here: ' +
+      '<a href="/api/auth/login" target="_blank">Login with Spotify</a>',
+    )
     .setVersion('1.0')
-    .addTag('spotify')
+    .addTag('auth')
+    .addTag('library')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   app.use(
-    '/docs',
+    `/${ DOC_PREFIX }`,
     apiReference({
       spec: {
         content: document,
@@ -32,10 +49,10 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.APP_PORT || 3001;
+  const port = process.env.APP_PORT || 4000;
 
   await app.listen(port);
-  logger.log(`Backend is running on: http://localhost:${ port }/api`);
-  logger.log(`API Reference is available at: http://localhost:${ port }/reference`);
+  logger.log(`Backend is running on: http://localhost:${ port }/${ API_PREFIX }`);
+  logger.log(`API Reference is available at: http://localhost:${ port }/${ DOC_PREFIX }`);
 }
 bootstrap();
